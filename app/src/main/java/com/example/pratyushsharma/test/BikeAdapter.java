1b7063430dbf660f2101e06fd74753fd89cfb788
VikramadityaPatil <vikramadityadpatil@outlook.com>
2017-03-11 20:03:40 +0530

    Created Functional bikeList view. No onClick method yet

--- app/src/main/java/com/example/pratyushsharma/test/BikeAdapter.java
+++ app/src/main/java/com/example/pratyushsharma/test/BikeAdapter.java
@@ -1,48 +1,26 @@
 package com.example.pratyushsharma.test;
 
 import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-/**
- * Created by Vikramaditya Patil on 11-03-2017.
- */
+import java.util.ArrayList;
 
-public class AndroidFlavorAdapter extends ArrayAdapter<AndroidFlavor> {
+public class BikeAdapter<T> extends ArrayAdapter<Bike> {
 
-    private static final String LOG_TAG = AndroidFlavorAdapter.class.getSimpleName();
-
-    /**
-     * This is our own custom constructor (it doesn't mirror a superclass constructor).
-     * The context is used to inflate the layout file, and the list is the data we want
-     * to populate into the lists.
-     *
-     * @param context        The current context. Used to inflate the layout file.
-     * @param androidFlavors A List of AndroidFlavor objects to display in a list
-     */
-    public AndroidFlavorAdapter(Activity context, ArrayList<AndroidFlavor> androidFlavors) {
-        // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
-        // the second argument is used when the ArrayAdapter is populating a single TextView.
-        // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
-        // going to use this second argument, so it can be any value. Here, we used 0.
-        super(context, 0, androidFlavors);
+    public BikeAdapter(Activity context, ArrayList<Bike> pbikeList) {
+        super(context, 0, pbikeList);
     }
 
-    /**
-     * Provides a view for an AdapterView (ListView, GridView, etc.)
-     *
-     * @param position The position in the list of data that should be displayed in the
-     *                 list item view.
-     * @param convertView The recycled view to populate.
-     * @param parent The parent ViewGroup that is used for inflation.
-     * @return The View for the position in the AdapterView.
-     */
+    @NonNull
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        // Check if the existing view is being reused, otherwise inflate the view
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View listItemView = convertView;
         if(listItemView == null) {
             listItemView = LayoutInflater.from(getContext()).inflate(
@@ -50,28 +28,17 @@ public class AndroidFlavorAdapter extends ArrayAdapter<AndroidFlavor> {
         }
 
         // Get the {@link AndroidFlavor} object located at this position in the list
-        AndroidFlavor currentAndroidFlavor = getItem(position);
+        Bike currentBike = getItem(position);
 
-        // Find the TextView in the list_item.xml layout with the ID version_name
-        TextView nameTextView = (TextView) listItemView.findViewById(R.id.version_name);
-        // Get the version name from the current AndroidFlavor object and
-        // set this text on the name TextView
-        nameTextView.setText(currentAndroidFlavor.getVersionName());
+        TextView addTextView = (TextView) listItemView.findViewById(R.id.bike_Add);
+        addTextView.setText(currentBike.getBikeAddress());
 
-        // Find the TextView in the list_item.xml layout with the ID version_number
-        TextView numberTextView = (TextView) listItemView.findViewById(R.id.version_number);
-        // Get the version number from the current AndroidFlavor object and
-        // set this text on the number TextView
-        numberTextView.setText(currentAndroidFlavor.getVersionNumber());
+        TextView priceTextView = (TextView) listItemView.findViewById(R.id.bike_Price);
+        priceTextView.setText(currentBike.getBikePrice());
 
-        // Find the ImageView in the list_item.xml layout with the ID list_item_icon
-        ImageView iconView = (ImageView) listItemView.findViewById(R.id.list_item_icon);
-        // Get the image resource ID from the current AndroidFlavor object and
-        // set the image to iconView
-        iconView.setImageResource(currentAndroidFlavor.getImageResourceId());
+        ImageView iconView = (ImageView) listItemView.findViewById(R.id.bike_Img);
+        iconView.setImageResource(currentBike.getBikeId());
 
-        // Return the whole list item layout (containing 2 TextViews and an ImageView)
-        // so that it can be shown in the ListView
         return listItemView;
     }
 
