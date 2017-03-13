8c952f599151597d68cc03e93ab0b30748060060
VikramadityaPatil <vikramadityadpatil@outlook.com>
2017-03-13 18:25:15 +0530

    Update Userinfo.java

--- app/src/main/java/com/example/pratyushsharma/test/Userinfo.java
+++ app/src/main/java/com/example/pratyushsharma/test/Userinfo.java
@@ -3,7 +3,7 @@ package com.example.pratyushsharma.test;
 
 
 public class Userinfo {
-    public String address;
+    public String mobile;
     public String username;
 
 
@@ -13,8 +13,21 @@ public class Userinfo {
     }
 
 
-    public Userinfo(String address, String username) {
-        this.address = address;
+    public Userinfo(String mobile, String username) {
+        this.mobile = mobile;
         this.username = username;
     }
+     public String getUsername() {		
+         return username;		
+     }
+    public void setUsername(String username) {		
+         this.username = username;		
+     }
+    public String getMobile() {		
+         return mobile;		
+     }
+    public void setMobile(String mobile) {		 +    public Userinfo(String address, String username) {
+         this.mobile = mobile;		 +        this.address = address;
+         this.username = username;
+      }
 }
