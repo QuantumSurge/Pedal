8c9ff3bf0a85231a7680f90eef4fd58b4f6de209
VikramadityaPatil <vikramadityadpatil@outlook.com>
2017-03-19 01:55:00 +0530

    Bike detail pop up created

--- app/src/main/java/com/example/pratyushsharma/test/ProfileFragment.java
+++ app/src/main/java/com/example/pratyushsharma/test/ProfileFragment.java
@@ -2,6 +2,7 @@ package com.example.pratyushsharma.test;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,13 +15,9 @@ import android.view.ViewGroup;
 public class ProfileFragment extends Fragment {
 
 
-    public ProfileFragment() {
-        // Required empty public constructor
-    }
-
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater,@Nullable ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
