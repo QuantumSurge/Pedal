b3f530b68f32005917cef9e167575ead57ab643c
VikramadityaPatil <vikramadityadpatil@outlook.com>
2017-03-25 17:08:52 +0530

    Layout for the add bike tab updated

--- app/src/main/java/com/example/pratyushsharma/test/AddBike.java
+++ app/src/main/java/com/example/pratyushsharma/test/AddBike.java
@@ -4,16 +4,48 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.NumberPicker;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+
+
 
 public class AddBike extends AppCompatActivity {
+
+    Spinner addressSpinner;
+    ArrayAdapter<CharSequence> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_bike);
+
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+        getWindow().setLayout( (int) (width*.9) , (int) (height*.7) );
+
+        addressSpinner  = (Spinner) findViewById(R.id.address_spinner);
+        adapter = ArrayAdapter.createFromResource(this,R.array.hostelNames,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        addressSpinner.setAdapter(adapter);
+        addressSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(AddBike.this,parent.getItemAtPosition(position)+" selected",Toast.LENGTH_LONG);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(AddBike.this,"Please select your hostel",Toast.LENGTH_LONG);
+            }
+        });
 
         NumberPicker dailyPicker = (NumberPicker) findViewById(R.id.dailyPicker);
         dailyPicker.setMaxValue(30);
@@ -30,12 +62,14 @@ public class AddBike extends AppCompatActivity {
         weeklyPicker.setMinValue(0);
         weeklyPicker.setWrapSelectorWheel(true);
 
-        DisplayMetrics dm = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(dm);
-        int width = dm.widthPixels;
-        int height = dm.heightPixels;
-        getWindow().setLayout( (int) (width*.9) , (int) (height*.7) );
-
+        ImageView bikeImage = (ImageView) findViewById(R.id.bike_Img);
+        bikeImage.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //Import image code goes here
+                Toast.makeText(AddBike.this,"upload image",Toast.LENGTH_LONG);
+            }
+        });
 
     }
 
--- app/src/main/java/com/example/pratyushsharma/test/Mainpage.java
+++ app/src/main/java/com/example/pratyushsharma/test/Mainpage.java
@@ -10,9 +10,6 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -65,11 +62,7 @@ public class Mainpage extends AppCompatActivity {
 
 
 
-        if (!calledAlready)
-        {
-            FirebaseDatabase.getInstance().setPersistenceEnabled(true);
-            calledAlready = true;
-        }*/
+
 
 
         mFirebaseAuth = FirebaseAuth.getInstance();
