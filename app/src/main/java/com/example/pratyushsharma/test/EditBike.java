83a1e832a961381244d0bc7851bbc7b56a0d0824
VikramadityaPatil <vikramadityadpatil@outlook.com>
2017-04-01 19:06:25 +0530

    Edit bike activity allows choosing images

--- app/src/main/java/com/example/pratyushsharma/test/EditBike.java
+++ app/src/main/java/com/example/pratyushsharma/test/EditBike.java
@@ -1,26 +1,66 @@
 package com.example.pratyushsharma.test;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.NumberPicker;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import static android.R.attr.name;
 import static android.os.Build.VERSION_CODES.M;
 import static com.example.pratyushsharma.test.R.id.dailyPicker;
 import static com.example.pratyushsharma.test.R.id.hourlyPicker;
+import static com.example.pratyushsharma.test.R.id.room;
+import static com.example.pratyushsharma.test.R.id.upload;
 import static com.example.pratyushsharma.test.R.id.weeklyPicker;
 import static java.security.AccessController.getContext;
 
 public class EditBike extends AppCompatActivity {
 
+    private Button edit;
+    private TextView name;
+    private TextView room;
+    private Spinner hostelSpinner;
+    private NumberPicker hourly;
+    private NumberPicker daily;
+    private NumberPicker weekly;
+    private DatabaseReference databasereference;
+    private FirebaseAuth firebaseAuth;
+    private ImageView bikeimg;
+    private Uri filePath;
+    private StorageReference uploadimg;
+    private ProgressDialog progress;
+    private int PICK_IMAGE_REQUEST = 4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_bike);
+        progress = new ProgressDialog(this);
+        setContentView(R.layout.activity_add_bike);
+        edit = (Button) findViewById(R.id.upload);
+        edit.setText("Edit");
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         Bundle bundle = getIntent().getExtras();
         String[] bikeAdd = bundle.getStringArray("Bike Address");
         String bikeName = bundle.getString("Bike Name");
@@ -54,6 +94,66 @@ public class EditBike extends AppCompatActivity {
         hourPicker.setValue(Integer.valueOf(hourPrice));
         dayPicker.setValue(Integer.valueOf(dayPrice));
         weekPicker.setValue(Integer.valueOf(weekPrice));
+
+        ImageView bikeImage = (ImageView) findViewById(R.id.bike_Img);
+        bikeImage.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                i.setType("image/*");
+                i.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(i, "Select an image"), PICK_IMAGE_REQUEST);
+                Toast.makeText(getBaseContext(),"upload image",Toast.LENGTH_LONG);
+            }
+        });
+
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (filePath != null) {
+
+                    progress.setMessage("Uploading...");
+                    progress.show();
+
+                    final StorageReference profileRef = uploadimg.child(user.getUid());
+
+
+                    profileRef.putFile(filePath).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            String mBikename = name.getText().toString();
+                            String mhostel = hostelSpinner.getSelectedItem().toString();
+                            String mroom= room.getText().toString();
+                            String mBikeAddress = mhostel + " " + mroom;
+                            int mHourly = hourly.getValue();
+                            int mDaily = daily.getValue();
+                            int mWeekly= weekly.getValue();
+                            String mUID = user.getUid();
+                            String mBoolean = "true";
+                            Price Price = new Price(mHourly,mDaily,mWeekly);
+
+                            Bike bike =new Bike(mBikename,mBikeAddress,mUID,mBoolean,Price);
+                            databasereference = FirebaseDatabase.getInstance().getReference();
+                            databasereference.child("Cycle").child(user.getUid()).setValue(bike);
+                            progress.dismiss();
+                            Toast.makeText(EditBike.this, "Uploaded", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(EditBike.this,ProfileFragment.class));
+                        }
+                    })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception exception) {
+                                    progress.dismiss();
+                                    Toast.makeText(EditBike.this, "Please try uploading the image again", Toast.LENGTH_SHORT).show();
+                                    EditBike.this.recreate();
+                                }
+                            });
+                }
+                progress.dismiss();
+
+            }
+        });
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
