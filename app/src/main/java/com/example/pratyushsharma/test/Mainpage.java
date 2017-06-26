0e7f755531f5b75cef7e484f460fbe232dabbf98
pratyushsharma <pratyush.sharma28@gmail.com>
2017-06-26 14:08:14 +0530

    Changed spinner range and added rupees sign

--- app/src/main/java/com/example/pratyushsharma/test/Mainpage.java
+++ app/src/main/java/com/example/pratyushsharma/test/Mainpage.java
@@ -78,6 +78,7 @@ public class Mainpage extends AppCompatActivity {
                     }
                 });
                 return true;
+
             default:
                 return super.onOptionsItemSelected(item);
         }
